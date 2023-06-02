9ea6b185c19c51ec376650d07555e58296b83806
sangeethnavodya <pererakasn.19@uom.lk>
2023-06-02 20:20:25 +0530

    Late init

--- server.js
+++ server.js
@@ -4,25 +4,7 @@ const app=express()
 const Sequelize=require('sequelize')  
 const dbConfig = require('./Config/dbConfig')
 
-const sequelize = new Sequelize(
-    dbConfig.DB,
-    dbConfig.USER,
-    dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect:dbConfig.dialect,
-    operatorsAliase:false,
-    pool:{
-        max:dbConfig.pool.max,
-        min:dbConfig.pool.min,
-        acquire:dbConfig.pool.acquire,
-        idle:dbConfig.pool.idle
-    }
-}
-)
 
-sequelize.authenticate().then(()=>{
-    console.log('Connnected to DataBase')
-}) 
 
 var corOption={
     origin:'https://localhost:8081'
